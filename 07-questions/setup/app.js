8c074deef44ac81528a07991e9d8e8afc372672d
Marco Kwan <marcokwan10@gmail.com>
2022-02-20 23:59:09 -0500

    up to 11

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,26 @@
 //using selectors inside the element
 // traversing the dom
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach((btn) => {
+// 	btn.addEventListener("click", function (e) {
+// 		const q = e.currentTarget.parentElement.parentElement;
+// 		q.classList.toggle("show-text");
+// 	});
+// });
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((q) => {
+	const btn = q.querySelector(".question-btn");
+	btn.addEventListener("click", function () {
+		questions.forEach((i) => {
+			if (i !== q) {
+				i.classList.remove("show-text");
+			}
+		});
+
+		q.classList.toggle("show-text");
+	});
+});
